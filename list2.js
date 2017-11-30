be8a8c5fc2ed372063e302c88f45e2d4855fcd10
Anthematics <jasonharder2@gmail.com>
2017-11-30 17:53:00 -0500

    fixed issue where for loop was not being called properly so array was not expanding

---
+++ list2.js
@@ -0,0 +1,44 @@
+
+class List {
+	constructor(size) {
+		 var DEFAULT_SIZE = 10;
+
+		 this.data = new Array(DEFAULT_SIZE);
+			 this.size = 0;
+	}
+
+	append(item) {
+		if (this.size >= this.data.length) {
+			this.growArray();
+			console.log("Fixed size array grew from" + this.size + "to" + this.data.length);
+		}
+			this.data[this.size] = item;
+			this.size++;
+	}
+
+	growArray() {
+		var newArray = new Array(this.data.length * 2);
+
+		for (var i = 0; i < this.data.length; i++) {
+			newArray[i] = this.data[i];
+		}
+		this.data = newArray;
+	}
+	get(index){
+		return this.data[index];
+		console.log("returning index")
+	}
+}
+
+const myList = new List();
+
+myList.append("yay!");
+for (var i = 0; i < 100; i++) {
+	myList.append(i);
+}
+
+console.log("Total size", myList.size);
+for (let i = 0; i < myList.size; i++) {
+	let value = myList.get(i);
+	console.log(value, "at index", i);
+}
